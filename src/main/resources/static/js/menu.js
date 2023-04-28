bf47b5985661d3873ae3d4f7b8b9245756d85da6
Divvy C. <54956345+DivvyCr@users.noreply.github.com>
2023-04-28 18:03:32 +0100

    Small-screen compatibility for: index, home.

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -2,10 +2,18 @@ const html = document.querySelector("html");
 const menuButton = document.getElementById("toggle-menu");
 const mobileMenuButton = document.getElementById("toggle-mobile-menu");
 
+if (window.matchMedia("(max-width: 600px)").matches) {
+    html.classList.toggle('menu-hidden');
+    html.classList.toggle('menu-shown');
+}
+
 menuButton.addEventListener('click', function sidebarToggle() {
     html.classList.toggle('menu-hidden');
+    html.classList.toggle('menu-shown');
 });
 
 mobileMenuButton.addEventListener('click', function sidebarToggle() {
     html.classList.toggle('menu-hidden');
+    html.classList.toggle('menu-shown');
 });
+
