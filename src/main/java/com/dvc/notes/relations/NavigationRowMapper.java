1a8c2a31ef0cee5cbd75cf3243c34122f1b6a5ca
Divvy C. <54956345+DivvyCr@users.noreply.github.com>
2023-04-29 18:58:33 +0100

    fixup! Redirect to book on chapter delete; some IntelliJ fixes.

--- src/main/java/com/dvc/notes/relations/NavigationRowMapper.java
+++ src/main/java/com/dvc/notes/relations/NavigationRowMapper.java
@@ -9,34 +9,34 @@ import org.springframework.jdbc.core.RowMapper;
 
 public class NavigationRowMapper implements RowMapper<Navigation> {
 
-    private HashMap<Integer, Navigation> idToNav = new HashMap<>();
-    
+    private final HashMap<Integer, Navigation> idToNav = new HashMap<>();
+
     @Override
     public Navigation mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-	Integer noteID = resultSet.getInt("chapterid");
-	String noteTitle = resultSet.getString("chapter_title");
-	String noteLink = resultSet.getString("link");
-	String notePath = resultSet.getString("agg_priority");
+        Integer noteID = resultSet.getInt("chapterid");
+        String noteTitle = resultSet.getString("chapter_title");
+        String noteLink = resultSet.getString("link");
+        String notePath = resultSet.getString("agg_priority");
 
-	ArrayList<Navigation> noteChildren = new ArrayList<>();
-	Object x = resultSet.getArray("children");
-	if (x != null) {
-	    Integer[] childrenIDs = (Integer[]) resultSet.getArray("children").getArray();
-	    for (Integer id : childrenIDs) {
-		noteChildren.add(idToNav.get(id));
-	    }
+        ArrayList<Navigation> noteChildren = new ArrayList<>();
+        Object x = resultSet.getArray("children");
+        if (x != null) {
+            Integer[] childrenIDs = (Integer[]) resultSet.getArray("children").getArray();
+            for (Integer id : childrenIDs) {
+                noteChildren.add(idToNav.get(id));
+            }
 
-	    noteChildren.sort((bn1, bn2) -> bn1.getPath().compareToIgnoreCase(bn2.getPath()));
-	}
+            noteChildren.sort((bn1, bn2) -> bn1.getPath().compareToIgnoreCase(bn2.getPath()));
+        }
 
-	// TODO: consider improving (?) whole navigation system
+        // TODO: consider improving (?) whole navigation system
 
-	Navigation newNav = new Navigation(noteID, noteTitle, noteLink, notePath, noteChildren);
+        Navigation newNav = new Navigation(noteID, noteTitle, noteLink, notePath, noteChildren);
 
-	this.idToNav.put(noteID, newNav);
+        this.idToNav.put(noteID, newNav);
 
-	int noteParent = resultSet.getInt("parent_chapterid");
-	return (noteParent.equals(noteID)) ? newNav : null;
+        int noteParent = resultSet.getInt("parent_chapterid");
+        return (noteParent == noteID) ? newNav : null;
     }
-    
+
 }
