ac3dfbf4bfe58fc0e54fd1831067918ae6acd2e6
Divvy C. <54956345+DivvyCr@users.noreply.github.com>
2023-04-30 01:35:48 +0100

    Change to DelegatingPasswordEncoder.

--- src/main/java/com/dvc/notes/AdminConfiguration.java
+++ src/main/java/com/dvc/notes/AdminConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -50,6 +50,6 @@ public class AdminConfiguration {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder(16);
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
