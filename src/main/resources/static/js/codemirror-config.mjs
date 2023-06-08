08fbfebea1e4fb8db4f670a9ce5e23d236226cad
Divvy C. <54956345+DivvyCr@users.noreply.github.com>
2023-06-08 20:07:19 +0100

    Implement custom parser to enable basic highlighting within LaTeX blocks.

--- src/main/resources/static/js/codemirror-config.mjs
+++ src/main/resources/static/js/codemirror-config.mjs
@@ -2,9 +2,11 @@ import {EditorState, StateField} from "@codemirror/state"
 import {drawSelection, EditorView} from "@codemirror/view"
 import {history} from "@codemirror/commands"
 import {syntaxHighlighting} from "@codemirror/language"
-import {tagHighlighter, tags} from "@lezer/highlight"
+import {styleTags, tagHighlighter, tags} from "@lezer/highlight"
 import {markdown, markdownLanguage} from "@codemirror/lang-markdown"
 import {vim} from "@replit/codemirror-vim"
+import {parseMixed} from "@lezer/common"
+import {parser as specialCharacterParser} from "../lezer/special-characters-lang.js"
 
 // COMPILE TO editor.bundle.js WITH THE FOLLOWING:
 // .\node_modules\.bin\rollup .\src\main\resources\static\js\codemirror-config.mjs -f iife -o .\src\main\resources\static\js\codemirror-editor.bundle.js -p @rollup/plugin-node-resolve
@@ -33,8 +35,21 @@ const mdHighlighter = tagHighlighter([
     {tag: tags.meta, class: "md-meta"},
     {tag: tags.labelName, class: "md-meta-label"},
     {tag: tags.regexp, class: "md-math"},
-    {tag: tags.operator, class: "md-math-delimiter"}
+    {tag: tags.operator, class: "md-math-delimiter"},
+    {tag: tags.controlOperator, class: "md-math-special"},
+    {tag: tags.annotation, class: "md-definition"},
 ]);
+
+const latexHighlightParser = specialCharacterParser.configure({
+    props: [
+        styleTags({
+            Expr: tags.emphasis,
+            Delimiter: tags.operator, // Parser Wrapper consumes delimiters from wrapped parser (ie. InlineLaTeX)?
+            Special: tags.controlOperator,
+            Any: tags.regexp
+        })
+    ]
+});
 
 const InlineLatexDelim = {resolve: "InlineLatex", mark: "InlineLatexMark"};
 const InlineLaTeX /* MarkdownConfig */ = {
@@ -48,18 +63,36 @@ const InlineLaTeX /* MarkdownConfig */ = {
     parseInline: [{
         name: "InlineLatexParser",
         parse(cx, next, pos) {
-            /* Handle OPENING delimiters: */
+            /* Latch onto OPENING delimiters: */
             if (next == 36 /* $ */ && cx.char(pos + 1) == 96 /* ` */) {
                 let startDelim = pos;
-                let endDelim = pos + 2;
-                /* Eagerly capture characters until either EOL is reached or an ending delimiter is found: */
-                while (endDelim < cx.end && (cx.char(endDelim) != 96 || cx.char(endDelim+1) != 36)) endDelim++;
-                if (endDelim >= cx.end) return -1; /* Ending delimiter not found. */
+                let lookaheadPos = pos + 2;
+                /* Eagerly capture characters until either EOL is reached or an ENDING delimiter is found: */
+                while (lookaheadPos < cx.end && (cx.char(lookaheadPos) != 96 || cx.char(lookaheadPos+1) != 36)) lookaheadPos++;
+                if (lookaheadPos >= cx.end) return -1; /* Ending delimiter not found. */
 
                 cx.addDelimiter(InlineLatexDelim, startDelim, startDelim + 2, true, false);
-                return cx.addDelimiter(InlineLatexDelim, endDelim, endDelim + 2, false, true);
+                return cx.addDelimiter(InlineLatexDelim, lookaheadPos, lookaheadPos + 2, false, true);
             }
+            return -1;
+        }
+    }],
+    wrap: parseMixed(node => {
+        return node.name === "InlineLatex" ? {parser: latexHighlightParser} : null
+    })
+};
 
+const CustomDefinition = {
+    defineNodes: [{
+        name: "Definition",
+        style: tags.annotation
+    }],
+    parseInline: [{
+        name: "CustomDefinitionParser",
+        parse(cx, next, pos) {
+            if (next == 36 /* $ */ && cx.char(pos + 1) == 36 /* $ */) {
+                return cx.addElement(cx.elt("Definition", pos, pos + 2));
+            }
             return -1;
         }
     }]
@@ -74,7 +107,7 @@ let startState = EditorState.create({
         syntaxHighlighting(mdHighlighter),
         syncFormText,
         vim(),
-        markdown({base: markdownLanguage, extensions: [InlineLaTeX]}),
+        markdown({base: markdownLanguage, extensions: [InlineLaTeX, CustomDefinition]}),
     ]
 });
 
